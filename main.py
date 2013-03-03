351c2a024a67f40331593910c7fe4074695da4eb
yoavz <yoavzimmerman@gmail.com>
2013-03-02 17:37:20 -0800

    webapp

--- main.py
+++ main.py
@@ -4,47 +4,17 @@ import os
 
 import json
 import urllib2
+import random
 
 
 import utils
 
-API_KEY = 'AIzaSyBYnrYdbRZnpvoGfUblBdcOx9njmUL6lGY'
-RADIUS = 1000
+API_KEY = 'AIzaSyBgGKN50nfIOuQnVgPpM6sFCYhAGsiwayU'
+PHOTO_URL = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=1600&sensor=true&key=%s&photoreference=' % (API_KEY)
+RADIUS = 10000
 
 jinja_environment = jinja2.Environment(autoescape=True,
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname	   (__file__), 'templates')))
-
-
-def geocode(query):
-	url = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false' % (urllib2.quote(query))
-	response = urllib2.urlopen(url)
-	json_raw = response.read()
-	json_data = json.loads(json_raw)
-
-	if json_data['status'] != 'OK':
-		return json_data['status']
-
-	else:
-		location = json_data['results'][0]['geometry']['location']
-		return '%s,%s' % (location['lat'], location['lng'])
-
-def nearbySearch(location, typeString):
-	location = urllib2.quote(location)
-
-	if location == None:
-		return 'Location not found!'
-
-	url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=%s&sensor=false&location=%s&radius=%s&types=%s' % (API_KEY, location, RADIUS, typeString)
-
-	response = urllib2.urlopen(url)
-	json_raw = response.read()
-	json_data = json.loads(json_raw)
-
-	if json_data["status"] != 'OK':
-		return json_data["status"]
-
-	#return json_data['results']
-	return (json_data['results'], url)
 
 
 def autocompleteSearch(keyword, location):
@@ -99,23 +69,137 @@ def radarSearch(location, typeString):
 
 
 
-# def delete_duplicates(results):
-# 	duplicateTypes = []
-# 	nonDuplicates = []
-# 	for result in results:
-# 		duplicate = False
-# 		typeList = result['types']
-# 		for i in range(len(typeList)):
-# 			if typeList[i] not in duplicateTypes:
-# 				duplicateTypes.append(typeList[i])
-# 				nonDuplicates.append(result)
-# 				break;
-# 	return nonDuplicates
 
 
 
 
 
+
+
+
+
+
+def geocode(query):
+	query = urllib2.quote(query)
+	url = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false' % (query)
+	response = urllib2.urlopen(url)
+	json_raw = response.read()
+	json_data = json.loads(json_raw)
+
+	if json_data['status'] != 'OK':
+		return json_data['status']
+
+	else:
+		location = json_data['results'][0]['geometry']['location']
+		return ('%s,%s' % (location['lat'], location['lng']), json_data['results'][0]['address_components'][0]['types'])
+
+
+
+
+def nearbySearch(location, typeString, radius):
+	location = urllib2.quote(location)
+
+	url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=%s&sensor=false&location=%s&radius=%s&types=%s' % (API_KEY, location, radius, typeString)
+
+	response = urllib2.urlopen(url)
+	json_raw = response.read()
+	json_data = json.loads(json_raw)
+
+	if json_data["status"] != 'OK':
+		return ({}, url)
+
+	#return json_data['results']
+	return (json_data['results'], url)
+
+
+
+
+def delete_duplicates(results):
+	#deletes duplicates of multiple stuff
+	duplicateTypes = []
+	nonDuplicates = []
+	for result in results:
+		typeList = result['types']
+		for i in range(len(typeList)):
+			if typeList[i] not in duplicateTypes:
+				duplicateTypes.append(typeList[i])
+				nonDuplicates.append(result)
+				break;
+	return nonDuplicates
+
+
+def filterType(results, typeString):
+	# return ref_ids for a type out of a results list
+	random.shuffle(results)
+	ref_ids = []
+	for result in results:
+		typeList = result['types']
+		for t in typeList:
+			if t in typeString:
+				ref_ids.append(result['reference'])
+				break;
+	return ref_ids
+
+def filterOnlyType(results, typeString):
+	random.shuffle(results)
+	ref_ids = []
+	for result in results:
+		flag = True
+		for t in result['types']:
+			if t not in typeString:
+				flag = False
+		if flag == True:
+			ref_ids.append(result['reference'])
+	return ref_ids
+
+def removeType(results, typeString):
+	for result in results:
+		for t in result['types']:
+			if t in typeString:
+				results.remove(result)
+	return results
+
+
+def getDetails(ref_id):
+	url = 'https://maps.googleapis.com/maps/api/place/details/json?&sensor=false&reference=%s&key=%s' % (ref_id, API_KEY)
+
+	response = urllib2.urlopen(url)
+	json_raw = response.read()
+	json_data = json.loads(json_raw)
+
+	if json_data["status"] != 'OK':
+		return ([], json_data['status'])
+
+	result = json_data['result']
+
+	return (result, json_data['status'])
+
+
+def getDetailsList(amount, ref_ids):
+	results = []
+	for i in range(amount):
+		results.append(getDetails(ref_ids[i])[0])
+	return results
+
+def getPhotoID(results):
+	for result in results:
+		if "photos" in result:
+			return random.choice(result['photos'])['photo_reference']
+	return False
+
+def photoURL(ref_id):
+	if not ref_id:
+		return 'img/placeholder.jpg'
+
+	return 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=1600&photoreference=%s&sensor=true&key=%s' % (ref_id, API_KEY)
+
+
+def addDefaultPhoto(results):
+	for result in results:
+		if not("photos" in result):
+			result['photos'] = [{'photo_reference': 'img/placeholder'}]
+	return results
+ 
 
 
 
@@ -131,23 +215,182 @@ class Handler(webapp2.RequestHandler):
         self.write(self.render_str(template, **kw))
 
 
-class MainPage(Handler):
+class TestPage(Handler):
 	def get(self):
 		typeString = utils.typeStringFromParameter(self.request.get('typestring'))
 		location = self.request.get('location')
-		coords = geocode(location)
-		search = nearbySearch(coords, typeString)
+		geocode_results = geocode(location)
+		coords = geocode_results[0]
+		radius = utils.localizer(geocode_results[1][0])
+		search = nearbySearch(coords, typeString, radius)
 		results = search[0]
+
+		random.shuffle(results) # to get different order everytime
+		results = delete_duplicates(results)
+		
 		url = search[1]
 
 		#nearby = delete_duplicates(nearby)
 		#text = textSearch(keyword, coords, typeString)
 		#autocomplete = autocompleteSearch(keyword, coords)
 		#radar = radarSearch(coords, typeString)
-		self.render('index.html', nearbyPlaces= results, url=url)
+		self.render('test.html', nearbyPlaces= results, url=url, radius=str(radius) + str(geocode_results[1]))
+	def post(self):
+		self.redirect('/')
+
+class itineraryPage(Handler):
+	def get(self):
+		typeString = utils.DEFAULT + '|' + utils.FOOD
+		location = self.request.get('location')
+
+		geocode_results = geocode(location)
+		coords = geocode_results[0]
+		radius = utils.localizer(geocode_results[1][0])
+
+		firstsearch = nearbySearch(coords, typeString, 50000)[0]
+		food = filterType(firstsearch, utils.FOOD)
+		fun = filterType(firstsearch, utils.DEFAULT)
+
+		if len(food) >= 3:
+			meals = getDetailsList(3, food)
+		else:
+			meals = ()
+
+		if len(fun) >= 3:
+			activities = getDetailsList(3, fun)
+		else:
+			activities = ()
+
+		photoref_id = getPhotoID(firstsearch)
+
+		self.render('day.html', radius = radius, meals = meals, activities = activities, photo = photoref_id)
 	def post(self):
 		self.redirect('/')
 
 
-app = webapp2.WSGIApplication([('/', MainPage)],
+
+class TitlePage(Handler):
+	def get(self):
+		error = self.request.get('error')
+		self.render('index.html', error = error)
+	def post(self):
+		query = self.request.get('main-search')
+		self.redirect('/day?query=%s' % (query))
+
+class DayPlanner(Handler):
+	def get(self):
+		typeString = utils.DEFAULT + '|' + utils.FOOD
+		location = self.request.get('query')
+
+		geocode_results = geocode(location)
+		coords = geocode_results[0]
+		radius = utils.localizer(geocode_results[1][0])
+
+		firstsearch = nearbySearch(coords, typeString, radius)[0]
+		food = filterType(firstsearch, utils.FOOD)
+		fun = filterType(firstsearch, utils.DEFAULT)
+
+		length = 3
+		if len(food) >= length:
+			meals = getDetailsList(length, food)
+		else:
+			meals = []
+
+		if len(fun) >= length:
+			activities = getDetailsList(length, fun)
+		else:
+			activities = []
+
+		photoref_id = getPhotoID(firstsearch)
+		if len(meals) > 0 or len(activities) > 0:
+			self.render('results.html', meals = meals, photo = photoref_id, location = location, places = activities, photo_url= PHOTO_URL)
+		else:
+			self.redirect('/?error=%s' % ("No matches for given location, please try again."))
+
+
+	def post(self):
+		typeString = utils.DEFAULT + '|' + utils.FOOD
+		location = self.request.get('query')
+
+		geocode_results = geocode(location)
+		coords = geocode_results[0]
+		radius = utils.localizer(geocode_results[1][0])
+
+		firstsearch = nearbySearch(coords, typeString, radius)[0]
+		food = filterType(firstsearch, utils.FOOD)
+		fun = filterType(firstsearch, utils.DEFAULT)
+
+		length = 3
+		if len(food) >= length:
+			meals = getDetailsList(length, food)
+		else:
+			meals = []
+
+		if len(fun) >= length:
+			activities = getDetailsList(length, fun)
+		else:
+			activities = []
+
+		photoref_id = getPhotoID(firstsearch)
+		if len(meals) > 0 or len(activities) > 0:
+			self.render('results.html', meals = meals, photo = photoref_id, location = location, places = activities, photo_url= PHOTO_URL)
+		else:
+			self.redirect('/')
+
+class ListPage(Handler):
+	def get(self):
+		activity_type = self.request.get('typestring')
+		typeString = utils.typeStringFromParameter(activity_type)
+		location = self.request.get('query')
+
+		geocode_results = geocode(location)
+		coords = geocode_results[0]
+		radius = utils.localizer(geocode_results[1][0])
+
+		places = nearbySearch(coords, typeString, radius)[0]
+
+		photoref_id = getPhotoID(places)
+
+		if (activity_type == 'default'):
+			activity_type = 'Adventures'
+
+		if len(places) > 0:
+			self.render('broad_results.html', location = location, activity_type= activity_type, places = places, photo = photoref_id, photo_url = PHOTO_URL)
+		else:
+			self.redirect('/')
+
+	def post(self):
+		typeString = utils.DEFAULT + '|' + utils.FOOD
+		location = self.request.get('query')
+
+		geocode_results = geocode(location)
+		coords = geocode_results[0]
+		radius = utils.localizer(geocode_results[1][0])
+
+		firstsearch = nearbySearch(coords, typeString, radius)[0]
+		food = filterType(firstsearch, utils.FOOD)
+		fun = filterType(firstsearch, utils.DEFAULT)
+
+		length = 3
+		if len(food) >= length:
+			meals = getDetailsList(length, food)
+		else:
+			meals = []
+
+		if len(fun) >= length:
+			activities = getDetailsList(length, fun)
+		else:
+			activities = []
+
+		photoref_id = getPhotoID(firstsearch)
+		if len(meals) > 0 or len(activities) > 0:
+			self.render('results.html', meals = meals, photo = photoref_id, location = location, places = activities, photo_url= PHOTO_URL)
+		else:
+			self.redirect('/?error=%s' % ("No matches for given location, please try again."))
+
+app = webapp2.WSGIApplication([('/test', TestPage),
+								('/itinerary', itineraryPage),
+								('/', TitlePage),
+								('/day', DayPlanner),
+								('/broad', ListPage)],
                               debug=True)
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ def createTypeString(string):
 	data = string.split()
 	return '|'.join(data)
 
-DEFAULT = createTypeString('zoo shopping_mall museum movie_theater aquarium art_gallery bicycle_store bowling_alley amusement_park')
+DEFAULT = createTypeString('zoo shopping_mall museum movie_theater aquarium art_gallery bicycle_store bowling_alley amusement_park park')
 
 FOOD = createTypeString('bakery cafe food restuarant meal_delivery meal_takeaway ')
 
@@ -15,9 +15,30 @@ ADULT = createTypeString('spa night_club liquor_store bar casino')
 def typeStringFromParameter(param):
 	if param == 'food': return FOOD
 	elif param == 'adult': return ADULT
-	elif param == 'feminine': return feminine
-	elif param == 'shopping': return shopping
+	elif param == 'feminine': return FEMININE
+	elif param == 'shopping': return SHOPPING
 	else: return DEFAULT
 
+
+def localizer(L):
+	if L == 'street_address': return 5*1600
+	elif L == 'street_number': return 5*1600
+	elif L == 'route': return 10*1600
+	elif L == 'intersection':return 2*1600
+	elif L == 'country': return 0
+	elif L == 'administrative_area_level_1': return 50000 
+	elif L == 'administrative_area_level_3': return 10*1600
+	elif L == 'administrative_area_level_2': return 20 * 1600
+	elif L == 'colloquial_area': return 10*1600
+	elif L == 'locality': return 10*1600
+	elif L == 'sublocality': return 5*1600
+	elif L == 'neighborhood': return 1*1600
+	elif L == 'premise': return 1*1600
+	elif L == 'subpremise': return 1*1600
+	elif L == 'postal_code': return 5*1600
+	elif L == 'park': return 2*1600
+	elif L == 'point_of_interest': return 5*1600
+
+	else: return 10*1600
 
 
